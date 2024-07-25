3084ecfcae5f58726df7b5dd361e345fe0208bfd
Qois Abdul Qudus <qoisabdulquduss@gmail.com>
2024-07-25 07:23:54 +0700

    Frontend-SerenityHub

--- src/components/admin/main.jsx
+++ src/components/admin/main.jsx
@@ -1,28 +1,31 @@
 /* eslint-disable no-undef */
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import L from 'leaflet';
-import { Chart } from 'react-google-charts';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import L from "leaflet";
+import { Chart } from "react-google-charts";
 
 const ReportCard = () => {
   const auth = useSelector((state) => state.auth);
   const [reports, setReports] = useState([]);
   const defaultCoord = {
-    latitude: '1.7575368113083125',
-    longitude: '115.65804619654459',
-    zoom: '1',
+    latitude: "1.7575368113083125",
+    longitude: "115.65804619654459",
+    zoom: "1",
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(`${import.meta.env.VITE_HOST_SERENITY}/admin/report/coordinates?limit=1000`, {
-        headers: {
-          Authorization: `Bearer ${auth.user ? auth.token : ''}`,
-        },
-      });
+      const result = await axios(
+        `${import.meta.env.VITE_HOST_SERENITY}/admin/report/coordinates?limit=1000`,
+        {
+          headers: {
+            Authorization: `Bearer ${auth.user ? auth.token : ""}`,
+          },
+        }
+      );
       setReports(result.data.data);
     };
     fetchData();
@@ -37,15 +40,16 @@ const ReportCard = () => {
 
   const MapMarker = (status) => {
     const markerColors = {
-      Menunggu: 'red',
-      Diproses: 'yellow',
-      Selesai: 'green',
+      Menunggu: "red",
+      Diproses: "yellow",
+      Selesai: "green",
     };
 
     // eslint-disable-next-line no-undef
     var newIcon = new L.Icon({
       iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${markerColors[status]}.png`,
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
@@ -55,70 +59,111 @@ const ReportCard = () => {
   };
 
   const chartData = [
-    ['Status', 'Count'],
-    ...Object.entries(statusCount).map(([status, count]) => [status, count]),
+    ["Status", "Count"],
+    ["Menunggu", statusCount.Menunggu],
+    ["Diproses", statusCount.Diproses],
+    ["Selesai", statusCount.Selesai],
   ];
 
   const chartOptions = {
-    pieHole: 0.4,
+    pieHole: 0.3,
     slices: [
       {
-        color: '#FFA500',
+        color: "red",
       },
       {
-        color: '#32CD32',
+        color: "gold",
       },
       {
-        color: '#FF0000',
+        color: "limegreen",
       },
     ],
-    chartArea: { width: '50%' },
+    chartArea: { width: "50%" },
     hAxis: {
       minValue: 0,
     },
     vAxis: {
-      title: 'Count',
+      title: "Count",
     },
   };
 
   return (
-    <div className='m-2 my-8 md:px-4'>
-      <div className='animate__fadeIn animate__animated animate__delay-0.5s box-border rounded-3xl bg-white px-4 py-8 drop-shadow md:p-12'>
-        <div className=''>
-          <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl capitalize'>Dashboard</h2>
+    <div className="m-2 my-8 md:px-4">
+      <div className="animate__fadeIn animate__animated animate__delay-0.5s box-border rounded-3xl bg-white px-4 py-8 drop-shadow md:p-12">
+        <div className="">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl capitalize">
+            Dashboard
+          </h2>
           <br></br>
           <div className="flex justify-between">
-          <h4 className="text-lg font-bold text-gray-900">Total Laporan : {totalCount}</h4>
+            <h4 className="text-lg font-bold text-gray-900">
+              Total Laporan : {totalCount}
+            </h4>
           </div>
           <Chart
             chartType="PieChart"
             data={chartData}
             options={chartOptions}
-            width="100%"
-            height="400px"
+            width="75%"
+            height="250px"
           />
-          <div className='grid grid-cols-1 gap-5 sm:grid-cols-4 mt-4'>
-            {Object.entries(statusCount).map(([status, count]) => (
-              <div key={status} className='group bg-white hover:bg-indigo-600 overflow-hidden shadow sm:rounded-lg cursor-pointer transition-all duration-100 ease-in'>
-                <div className='px-4 py-5 sm:p-6'>
-                  <dl>
-                    <dt className='text-sm leading-5 font-medium text-gray-500 group-hover:text-white truncate'>{status}</dt>
-                    <dd className='mt-1 text-3xl leading-9 font-semibold text-indigo-600 group-hover:text-white'>{count}
-                    </dd>
-                  </dl>
+          <div className="grid grid-cols-1 gap-5 sm:grid-cols-4 mt-4">
+            {Object.entries(statusCount)
+              .sort((a, b) => {
+                const statusOrder = {
+                  Menunggu: 0,
+                  Diproses: 1,
+                  Selesai: 2,
+                };
+                return statusOrder[a[0]] - statusOrder[b[0]];
+              })
+              .map(([status, count]) => (
+                <div
+                key={status}
+                className={`group overflow-hidden shadow sm:rounded-lg transition-all duration-100 ease-in
+                  ${status === 'Menunggu' ? 'bg-red-500' :
+                   status === 'Diproses' ? 'bg-yellow-500' :
+                   status === 'Selesai' ? 'bg-green-500' : 'bg-white hover:bg-green-600'}
+                `}
+              >
+                  <div className="px-4 py-5 sm:p-6">
+                    <dl>
+                      <dt className="text-sm leading-5 font-medium text-white truncate">
+                        {status}
+                      </dt>
+                      <dd className="mt-1 text-3xl leading-9 font-semibold text-white truncate">
+                        {count}
+                      </dd>
+                    </dl>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
         <br></br>
-        <h4 className='text-3xl font-extrabold tracking-tight text-gray-900  capitalize'>Lokasi Laporan</h4>
-        <div className='col-span-1 my-4'>
-          <MapContainer center={[defaultCoord.latitude, defaultCoord.longitude]} zoom={5} scrollWheelZoom={true} className='h-64 w-full mt-4'>
-            <TileLayer attribution='&copy; <a n href="http://osm.org/copyright">OpenStreetMap</a>' url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        <h4 className="text-3xl font-extrabold tracking-tight text-gray-900  capitalize">
+          Lokasi Laporan
+        </h4>
+        <div className="col-span-1 my-4">
+          <MapContainer
+            center={[defaultCoord.latitude, defaultCoord.longitude]}
+            zoom={5}
+            scrollWheelZoom={true}
+            className="h-64 w-full mt-4"
+          >
+            <TileLayer
+              attribution='&copy; <a n href="http://osm.org/copyright">OpenStreetMap</a>'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
             {reports.map((report) => (
               <div key={report._id}>
-                <Marker key={report._id} position={[report.latitude, report.longitude]} status={report.status} icon={MapMarker(report.status)} className='bg-red-200'>
+                <Marker
+                  key={report._id}
+                  position={[report.latitude, report.longitude]}
+                  status={report.status}
+                  icon={MapMarker(report.status)}
+                  className="bg-red-200"
+                >
                   <Popup>
                     <div>{report.status}</div>
                     <Link to={`report/detail/${report._id}`}>Detail</Link>
